76edc04a466683436622cf4d90b1eb483574ee40
Alessander <alessander.logistica@gmail.com>
2020-03-21 23:43:39 -0300

    set the HomeController as a Controller

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,16 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.service.CoronaVirusDataService;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	CoronaVirusDataService coronaVirusDataService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
-		
-		return "home";
-	}
+    public String home(Model model) {
+        return "home";
+    }
+	
 }
